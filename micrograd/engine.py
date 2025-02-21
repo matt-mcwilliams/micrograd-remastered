fdbed2a448897874808b4e1ab984a13a987d48ca
Matthew McWilliams (School Laptop) <mcwilliams38229@students.apsb.org>
2025-02-20 21:35:12 -0600

    implement backpropogation in Value, add tests to validate

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,29 +2,114 @@ import math
 
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, _prev=(), _op=''):
         self.data = data
+        self.grad = 0.0
+        self._prev = _prev
+        self._op = _op
+        self._backward = lambda: None
+    
+    def __repr__(self):
+        return f'Value({self.data})'
+    
+    def backward(self):
+        topo = [] # this will contain all the sorted values
+        stack = [self]
+        while len(stack) > 0:
+            stack[0].grad = 0.0
+            if len([g for g in stack[0]._prev if g not in topo]) > 0:
+                stack = list(stack[0]._prev) + stack
+                stack = [i for n, i in enumerate(stack) if i not in stack[:n] and i not in topo] # remove duplicates
+                continue
+            topo.append(stack.pop(0))
+        
+        self.grad = 1.0
+
+        for v in reversed(topo):
+            v._backward()
+
     
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, _prev=(self,other), _op='+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
 
         return out
     
     def __radd__(self, other):  # other + self
         return self + other
     
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __rsub__(self, other):  # other - self
+        return other + (-self)
+    
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, _prev=(self,other), _op='*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward =_backward
 
         return out
     
     def __rmul__(self, other):  # other * self
         return self * other
     
+    def __pow__(self, k):
+        assert isinstance(k, (int, float)), "Only float/integer exponents for now!"
+        out = Value(self.data**k, _prev=(self,), _op=f'**{k}')
+
+        def _backward():
+            self.grad = k * (self.data ** (k-1) * out.grad)
+        out._backward = _backward
+
+        return out
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __rtruediv__(self, other):      # other / self
+        return other * self**-1
+    
+    def exp(self):
+        n = math.exp(self.data)
+        out = Value(n, _prev=(self,), _op='exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+
+        return out
+    
     def tanh(self):
         x = self.data
-        out = Value(math.tanh(x))
+        n = math.tanh(x)
+        out = Value(n, _prev=(self,), _op='tanh')
+
+        def _backward():
+            self.grad += (1 - n ** 2) * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def relu(self):
+        x = self.data
+        n = x * int(x>0)
+        out = Value(n, _prev=(self,), _op='relu')
+
+        def _backward():
+            self.grad += int(x>0)
+        out._backward = _backward
 
         return out
--- test/test_engine.py
+++ test/test_engine.py
@@ -69,32 +69,115 @@ class TestValue(unittest.TestCase):
         tb = 0.4 * ta
 
         self.assertAlmostEqual(b.data, tb.item(), 4)    
+    
+    def test_tanh(self):
+        a = Value(9.8)
+        b = a.tanh()
 
-    def test_neuron(self):
+        ta = torch.Tensor([9.8]).double();      ta.requires_grad = True
+        tb = ta.tanh()
+
+        self.assertAlmostEqual(b.data, tb.item(), 4)
+    
+    def test_tanh_alt(self):
+        a = Value(9.8)
+        b = (2*a).exp()
+        c = (b-1)/(b+1)
+
+        c.backward()
+
+        ta = torch.Tensor([9.8]).double();      ta.requires_grad = True
+        tb = ta.tanh()
+
+        tb.backward()
+
+        self.assertAlmostEqual(c.data, tb.item(), 4)
+        self.assertAlmostEqual(a.grad, ta.grad.item(), 4)
+
+    def test_neuron_value(self):
         x1 = Value(2.0)
         w1 = Value(-3.0)
         x2 = Value(0.0)
         w2 = Value(1.0)
-        b = Value(-1.0)
+        b = Value(6.881374)
         x1w1 = x1 * w1
         x2w2 = x2 * w2
         x1w1x2w2 = x1w1 + x2w2
         n = x1w1x2w2 + b
         o = n.tanh()
 
-        tx1 = torch.Tensor([2.0]).double()
-        tw1 = torch.Tensor([-3.0]).double()
-        tx2 = torch.Tensor([0.0]).double()
-        tw2 = torch.Tensor([1.0]).double()
-        tb = torch.Tensor([-1.0]).double()
+        o.backward()  # TODO: Add tests for backward pass
+
+        tx1 = torch.Tensor([2.0]).double();         tx1.requires_grad = True
+        tw1 = torch.Tensor([-3.0]).double();        tw1.requires_grad = True
+        tx2 = torch.Tensor([0.0]).double();         tx2.requires_grad = True
+        tw2 = torch.Tensor([1.0]).double();         tw2.requires_grad = True
+        tb = torch.Tensor([6.881374]).double();     tb.requires_grad = True
         tx1w1 = tx1 * tw1
         tx2w2 = tx2 * tw2
         tx1w1x2w2 = tx1w1 + tx2w2
         tn = tx1w1x2w2 + tb
         to = tn.tanh()
 
+        to.backward()
+
         self.assertAlmostEqual(o.data, to.item(), 4)
 
+        self.assertAlmostEqual(x1.grad, tx1.grad.item(), 4)
+        self.assertAlmostEqual(x2.grad, tx2.grad.item(), 4)
+        self.assertAlmostEqual(w1.grad, tw1.grad.item(), 4)
+        self.assertAlmostEqual(w2.grad, tw2.grad.item(), 4)
+        self.assertAlmostEqual(b.grad, tb.grad.item(), 4)
+        
+    def test_relu(self):
+
+        a = Value(4.0)
+        b = a.relu()
+
+        b.backward()
+
+        ta = torch.Tensor([4.0]).double();    ta.requires_grad = True
+        tb = ta.relu()
+
+        tb.backward()
+
+        self.assertAlmostEqual(b.data, tb.item(), 4)
+        self.assertAlmostEqual(a.grad, ta.grad.item(), 4)
+    
+    def test_comprehensive(self):
+        a = Value(-4.0)
+        b = Value(2.0)
+        c = a + b
+        d = a * b + b**3
+        c += c + 1
+        c += 1 + c + (-a)
+        d += d * 2 + (b + a).relu()
+        d += 3 * d + (b - a).relu()
+        e = c - d
+        f = e**2
+        g = f / 2.0
+        g += 10.0 / f
+
+        g.backward()
+
+        ta = torch.Tensor([-4.0]).double();     ta.requires_grad = True
+        tb = torch.Tensor([2.0]).double();      tb.requires_grad = True
+        tc = ta + tb
+        td = ta * tb + tb**3
+        tc += tc + 1
+        tc += 1 + tc + (-ta)
+        td += td * 2 + (tb + ta).relu()
+        td += 3 * td + (tb - ta).relu()
+        te = tc - td
+        tf = te**2
+        tg = tf / 2.0
+        tg += 10.0 / tf
+
+        tg.backward()
+
+        self.assertAlmostEqual(g.data, tg.item(), 4)
+        self.assertAlmostEqual(a.grad, ta.grad.item(), delta=10)  # use delta because imprecise at large numbers
+        self.assertAlmostEqual(b.grad, tb.grad.item(), delta=10)
 
 
 
